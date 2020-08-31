75389e099aa89001c22b58d146fd95c442bc9166
Kalhara Perera <adorekasun@gmail.com>
2020-08-31 16:37:10 +0530

    Working : Getting User Inputs through URL

--- Sample-Web-App/Sample-Web-App/src/main/java/com/adore96/SampleWebApp/Controller/HomeController.java
+++ Sample-Web-App/Sample-Web-App/src/main/java/com/adore96/SampleWebApp/Controller/HomeController.java
@@ -1,14 +1,25 @@
 package com.adore96.SampleWebApp.Controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String print(){
+    public String print(HttpServletRequest request){
+        HttpSession httpSession = request.getSession();
+
+        String name = request.getParameter("name");
         System.out.println("home method called");
+        System.out.println("name  "+name);
+
+        httpSession.setAttribute("name",name);
+
         return "HomePage";
     }
 }
