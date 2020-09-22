3a868bec0f15a2b5f718034dc9b45baf3b7ecf0a
Kalhara Perera <adorekasun@gmail.com>
2020-09-22 09:56:56 +0530

    Added SpringAngularWebApp

---
+++ SpringAngularWebApp/SpringAngularWebApp/src/main/java/com/adore96/SpringAngularWebApp/SpringAngularWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.adore96.SpringAngularWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularWebAppApplication.class, args);
+	}
+
+}
---
+++ SpringAngularWebApp/SpringAngularWebApp/src/test/java/com/adore96/SpringAngularWebApp/SpringAngularWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adore96.SpringAngularWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
