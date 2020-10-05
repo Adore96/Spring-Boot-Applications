98bf5f3c18f9e49eea24a1aee5537f56cf849f4f
Kalhara Perera <adorekasun@gmail.com>
2020-10-05 13:49:58 +0530

    added JwtApi

--- BackEnd-SpringWebApp/SpringAngularWebApp/src/main/java/com/adore96/SpringAngularWebApp/securityConfig/AppSecurityConfig.java
+++ BackEnd-SpringWebApp/SpringAngularWebApp/src/main/java/com/adore96/SpringAngularWebApp/securityConfig/AppSecurityConfig.java
@@ -1,31 +1,40 @@
 package com.adore96.SpringAngularWebApp.securityConfig;
 
 import com.adore96.SpringAngularWebApp.filter.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.RedirectView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
-public class appSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
-
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
@@ -47,7 +56,12 @@ public class appSecurityConfig extends WebSecurityConfigurerAdapter implements W
                 .exceptionHandling().accessDeniedPage("/403")
                 .and()
                 .formLogin()
-                .loginPage("/login").permitAll()
+                .loginProcessingUrl("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .successHandler(this::loginSuccessHandler)
+                .failureHandler(this::loginFailureHandler)
+//                .loginPage("/login").permitAll()
                 .and()
                 .logout().invalidateHttpSession(true)
                 //clear all the data when logout
@@ -71,5 +85,25 @@ public class appSecurityConfig extends WebSecurityConfigurerAdapter implements W
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    private RedirectView loginSuccessHandler(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication) throws IOException {
+
+        response.setStatus(HttpStatus.OK.value());
+        objectMapper.writeValue(response.getWriter(), "Yayy you logged in!");
+        return new RedirectView("/");
+    }
+
+    private RedirectView loginFailureHandler(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException e) throws IOException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        objectMapper.writeValue(response.getWriter(), "Nopity nop!");
+        return new RedirectView("/login");
     }
 }
