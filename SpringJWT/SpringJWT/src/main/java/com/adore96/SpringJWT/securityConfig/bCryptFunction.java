972d80dca13cd13fbfa2b27ab9f96b896709d2f7
Kalhara Perera <adorekasun@gmail.com>
2020-12-25 23:32:36 +0530

    Update bCryptFunction.java

--- SpringJWT/SpringJWT/src/main/java/com/adore96/SpringJWT/securityConfig/bCryptFunction.java
+++ SpringJWT/SpringJWT/src/main/java/com/adore96/SpringJWT/securityConfig/bCryptFunction.java
@@ -1,9 +1,20 @@
 package com.adore96.SpringJWT.securityConfig;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 /**
  * @author kalharaperera ON 12/22/20
  * @project SpringJWT
  */
 
+@Service
 public class bCryptFunction {
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
