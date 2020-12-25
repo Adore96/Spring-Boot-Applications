5a434db50aeff48318030328f5050c5ce252394d
Kalhara Perera <adorekasun@gmail.com>
2020-12-25 23:38:55 +0530

    Update appSecurityConfig.java

--- SpringJWT/SpringJWT/src/main/java/com/adore96/SpringJWT/securityConfig/appSecurityConfig.java
+++ SpringJWT/SpringJWT/src/main/java/com/adore96/SpringJWT/securityConfig/appSecurityConfig.java
@@ -1,6 +1,13 @@
 package com.adore96.SpringJWT.securityConfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * @author kalharaperera ON 12/22/20
@@ -9,5 +16,38 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 public class appSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/login").permitAll()
+                .antMatchers("/Newuser").permitAll()
+                .antMatchers("/").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers("/EditStudent/**").hasAuthority("ADMIN")
+                .antMatchers("/DeleteStudent/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().accessDeniedPage("/403")
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().invalidateHttpSession(true)
+                //clear all the data when logout
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll();
+    }
 
 }
